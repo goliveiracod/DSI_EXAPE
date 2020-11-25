cab194ef92cf702af83d64736bd85d5f476227cb
Gabriel Oliveira <goliveiracod@gmail.com>
2020-11-25 01:48:18 -0300

    added database script and table image

---
+++ database/database.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE dsi_exape;
+
+USE dsi_exape;
+
+CREATE TABLE contacts (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  primary_cellphone VARCHAR(20) NOT NULL,
+  secondary_cellphone VARCHAR(20),
+  primary_phone VARCHAR(20),
+  secondary_phone VARCHAR(20),
+  CONSTRAINT id_pk PRIMARY KEY (id)
+);
